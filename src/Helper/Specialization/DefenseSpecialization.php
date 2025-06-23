2112aa764eb9873d51c4c66e7b5cf0816f2e82ef
dscheinah <github@jb.chmann.net>
2025-06-23 09:18:29 +0200

    increase early defense specializations

--- src/Helper/Specialization/DefenseSpecialization.php
+++ src/Helper/Specialization/DefenseSpecialization.php
@@ -6,7 +6,7 @@ class DefenseSpecialization implements SpecializationInterface
 {
     public static function battle(array $player, int $duration): array
     {
-        $player['defense'] += 2 * (int) (1.1 ** $duration);
+        $player['defense'] += 4 * (int) (1.1 ** $duration);
         return $player;
     }
 }
--- src/Helper/Specialization/DefensiveMagicSpecialization.php
+++ src/Helper/Specialization/DefensiveMagicSpecialization.php
@@ -6,7 +6,7 @@ class DefensiveMagicSpecialization implements SpecializationInterface
 {
     public static function battle(array $player, int $duration): array
     {
-        $player['magic_defense'] += 2 * (int) (1.1 ** $duration);
+        $player['magic_defense'] += 4 * (int) (1.1 ** $duration);
         return $player;
     }
 }
