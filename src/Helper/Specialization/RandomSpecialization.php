ae0cf8139aeafe31407b7e2d852571922d20938d
dscheinah <github@jb.chmann.net>
2025-09-25 23:35:47 +0200

    fix random specialization

--- src/Helper/Specialization/RandomSpecialization.php
+++ src/Helper/Specialization/RandomSpecialization.php
@@ -7,7 +7,7 @@ class RandomSpecialization implements SpecializationInterface
     public static function battle(array $player, int $duration): array
     {
         foreach ($player as $key => $value) {
-            $player[$key] += (mt_srand() % 3) - 1;
+            $player[$key] += (mt_rand() % 3) - 1;
             if ($player[$key] < 0) {
                 $player[$key] = 0;
             }
