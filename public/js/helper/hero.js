144f3b0a2e98aba72ddd87100b16e6f6565200ab
dscheinah <github@jb.chmann.net>
2025-10-22 22:52:29 +0200

    display all tier stats

--- public/js/helper/hero.js
+++ public/js/helper/hero.js
@@ -1,8 +1,9 @@
 import escape from './escape.js';
+import league from './league.js';
 import tags from './tags.js';
 
 function compareForClassString(key, reference, data) {
-    if (!reference || reference[key] === data[key]) {
+    if (!reference || !data || reference[key] === data[key]) {
         return '';
     }
     return reference[key] > data[key] ? 'sx-error' : 'sx-highlight';
@@ -11,13 +12,31 @@ function compareForClassString(key, reference, data) {
 export default function (hero, compare) {
     const specialization = hero.specialization ? `&ndash; ${hero.specialization.icon} ${hero.specialization.name}` : '';
     const shrine = hero.shrine ? `&ndash; ${hero.shrine.icon} ${hero.shrine.text}` : '';
-    return `<span class="hero">
-        ${hero.active ? '⭐️' : ''} <strong>${escape(hero.name)}</strong> <span>${shrine}</span> <span>${specialization}</span><br/>
-        <span class="hero-stat ${compareForClassString('health', compare, hero)}">${tags(['health'])} ${hero.health}</span>
-        <span class="hero-stat ${compareForClassString('damage', compare, hero)}">${tags(['damage'])} ${hero.damage}</span>
-        <span class="hero-stat ${compareForClassString('defense', compare, hero)}">${tags(['defense'])} ${hero.defense}</span>
-        <span class="hero-stat ${compareForClassString('magic', compare, hero)}">${tags(['magic'])} ${hero.magic}</span>
-        <span class="hero-stat ${compareForClassString('speed', compare, hero)}">${tags(['speed'])} ${hero.speed}</span>
-        <span class="hero-stat ${compareForClassString('curse', compare, hero)}">${tags(['curse'])} ${hero.curse}</span><br/>
-    </span>`;
+    const tiers = [];
+    hero.tiers.forEach((tier, index) => tiers.push(`<tr>
+        <th>${league(index, 'icon')}</th>
+        <td class="${compareForClassString('health', compare?.tiers[index], tier)}">${tier.health}</td>
+        <td class="${compareForClassString('damage', compare?.tiers[index], tier)}">${tier.damage}</td>
+        <td class="${compareForClassString('defense', compare?.tiers[index], tier)}">${tier.defense}</td>
+        <td class="${compareForClassString('magic', compare?.tiers[index], tier)}">${tier.magic}</td>
+        <td class="${compareForClassString('speed', compare?.tiers[index], tier)}">${tier.speed}</td>
+        <td class="${compareForClassString('curse', compare?.tiers[index], tier)}">${tier.curse}</td>
+    </tr>`));
+    return `
+        <p class="hero">
+            ${hero.active ? '⭐️' : ''} <strong>${escape(hero.name)}</strong> <span>${shrine}</span> <span>${specialization}</span>
+        </p>
+        <table class="hero-table">
+            <tr>
+                <th></th>
+                <th>${tags(['health'])}</th>
+                <th>${tags(['damage'])}</th>
+                <th>${tags(['defense'])}</th>
+                <th>${tags(['magic'])}</th>
+                <th>${tags(['speed'])}</th>
+                <th>${tags(['curse'])}</th>
+            </tr>
+            ${tiers.join('')}
+        </table>
+    `;
 }
